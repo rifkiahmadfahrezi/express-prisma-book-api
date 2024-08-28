72637a8a61d1e44faaf904c88ac4b666deb563dd
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-08-28 07:26:55 +0700

    refactor: move index file

---
+++ api/index.ts
@@ -0,0 +1,37 @@
+import express , { Express, NextFunction, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import bookRoute from '../src/routes/book.routes'
+import authorRoute from '../src/routes/author.routes'
+import bodyParser = require('body-parser')
+
+dotenv.config()
+
+const app : Express = express()
+const port = process.env.PORT
+
+app.get('',  (( _ , res : Response) => {
+  res.status(200).json({
+    status: true,
+    message: "Hello there 👋!"
+  })
+}))
+app.use(express.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json())
+app.use(authorRoute)
+app.use(bookRoute)
+
+app.use((error: any, _req: Request, res: Response, _next: NextFunction) => {
+  res.status(501).json({
+    status: false,
+    message: "An error occurred",
+    error,
+  })
+})
+
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+}) 

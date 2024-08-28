5e1f392e705ce13fd26b6d7c897f9b00e8d2c9a8
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-08-29 06:12:22 +0700

    feat: protecting routes using authMiddleware on Create, Update and Delete operations

--- src/routes/author.routes.ts
+++ src/routes/author.routes.ts
@@ -2,14 +2,16 @@ import { Router } from "express";
 import author from "../controllers/author";
 import { validateSchema } from "../middlewares/validateSchema";
 import { createAuthorSchema, updateAuthorSchema } from "../schemas/author";
+import { authMiddleware } from "../middlewares/authMiddleware";
+
 
 const authorRoute = Router()
 
-authorRoute.post('/authors', validateSchema(createAuthorSchema) ,author.create)
+authorRoute.post('/authors', authMiddleware,  validateSchema(createAuthorSchema) ,author.create)
 authorRoute.get('/authors', author.getAll)
 authorRoute.get('/authors/:authorId', author.getById)
-authorRoute.put('/authors/:authorId', validateSchema(updateAuthorSchema), author.update)
-authorRoute.delete('/authors/:authorId', author.delete)
+authorRoute.put('/authors/:authorId', authMiddleware,  validateSchema(updateAuthorSchema), author.update)
+authorRoute.delete('/authors/:authorId', authMiddleware,  author.delete)
 
 
 export default authorRoute
--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -2,14 +2,15 @@ import { Router } from "express";
 import book from "../controllers/book";
 import { validateSchema } from "../middlewares/validateSchema";
 import { createBookSchema, updateBookSchema } from "../schemas/book";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const bookRoute = Router()
 
-bookRoute.post('/books', validateSchema(createBookSchema), book.create)
+bookRoute.post('/books', authMiddleware, validateSchema(createBookSchema), book.create)
 bookRoute.get('/books', book.getAll)
 bookRoute.get('/books/:bookId', book.getById)
-bookRoute.put('/books/:bookId', validateSchema(updateBookSchema), book.update)
-bookRoute.delete('/books/:bookId', book.delete)
+bookRoute.put('/books/:bookId', authMiddleware, validateSchema(updateBookSchema), book.update)
+bookRoute.delete('/books/:bookId', authMiddleware, book.delete)
 
 
 export default bookRoute

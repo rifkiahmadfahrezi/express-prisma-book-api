edaea2b60305d4598b5df522306f782a1c542603
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-08-29 08:58:19 +0700

    feat: setup cors allow all origin

--- api/index.ts
+++ api/index.ts
@@ -4,12 +4,14 @@ import bookRoute from '../src/routes/book.routes'
 import authorRoute from '../src/routes/author.routes'
 import bodyParser = require('body-parser')
 import authRoute from '../src/routes/auth.routes'
+import cors from 'cors'
 
 dotenv.config()
 
 const app : Express = express()
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.get('',  (( _ , res : Response) => {
   res.status(200).json({
     status: true,
@@ -27,18 +29,6 @@ app.use(authRoute)
 app.use(authorRoute)
 app.use(bookRoute)
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token"
-  );
-  if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).json({});
-  }
-  next();
-})
 app.use((error: any, _req: Request, res: Response, _next: NextFunction) => {
   res.status(501).json({
     status: false,
